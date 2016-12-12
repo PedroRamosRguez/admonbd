61f29b87008d63596cffcce6230850ddbf213ab4
Orlandy Ariel Sánchez <alu0100773408@ull.edu.es>
2016-12-12 17:32:51 +0000

    RE:script con trigger, quitado delimiter

--- Ej_regalos/trigger.sql
+++ Ej_regalos/trigger.sql
@@ -1,9 +1,8 @@
-USE regalos;
-DELIMITER $$
+
 DROP TRIGGER IF EXISTS precioTotal;
 CREATE TRIGGER precioTotal before insert on REGALO
 FOR EACH ROW
 BEGIN 
 ALTER TABLE REGALO ADD Precio_Total FLOAT NOT NULL 
+SET new.Precio_Total = new.Cantidad_articulo * new.Precio_unidad;
 END $$
-DELIMITER;
