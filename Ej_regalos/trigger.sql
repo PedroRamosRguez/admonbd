f116a008bce075bacfe0e103ec62c361c07e1a95
Orlandy Ariel Sánchez <alu0100773408@ull.edu.es>
2016-12-12 17:29:06 +0000

    RE:script con trigger, quitado set

--- Ej_regalos/trigger.sql
+++ Ej_regalos/trigger.sql
@@ -5,6 +5,5 @@ CREATE TRIGGER precioTotal before insert on REGALO
 FOR EACH ROW
 BEGIN 
 ALTER TABLE REGALO ADD Precio_Total FLOAT NOT NULL 
-SET new.Precio_Total = new.Cantidad_articulo * new.Precio_unidad;
 END $$
 DELIMITER;
