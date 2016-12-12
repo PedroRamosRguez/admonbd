4b3b7f30a3fdbc7b96fb4883da07e171134a9539
Orlandy Ariel Sánchez <alu0100773408@ull.edu.es>
2016-12-12 18:11:41 +0000

    regalos done

--- Ej_regalos/trigger.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER $$
-DROP TRIGGER IF EXISTS precioTotal;
-CREATE TRIGGER precioTotal before insert on REGALO
-FOR EACH ROW
-BEGIN  
-SET new.Precio_Total = new.Cantidad_articulo * new.Precio_unidad;
-END$$
-DELIMITER;
