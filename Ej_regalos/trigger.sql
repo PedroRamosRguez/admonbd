b3725cdf7db15b6f167521a75463d55137d6372b
Orlandy Ariel Sánchez <alu0100773408@ull.edu.es>
2016-12-12 17:21:53 +0000

    RE:script con trigger

--- Ej_regalos/trigger.sql
+++ Ej_regalos/trigger.sql
@@ -1,7 +1,9 @@
+DELIMITER $$
 DROP TRIGGER IF EXISTS precioTotal;
 CREATE TRIGGER precioTotal before insert on REGALO
 FOR EACH ROW
 BEGIN 
 ALTER TABLE REGALO ADD Precio_Total FLOAT NOT NULL 
 SET new.Precio_Total = new.Cantidad_articulo * new.Precio_unidad;
-END
+END $$
+DELIMITER;
